0a8b76a1fefab4ee4760f5cc05741175dcd8b0cd
yangzhaohu <102209057+yangzhaohu@users.noreply.github.com>
2023-04-04 09:17:21 +0000

    feat: queue feature

--- sio_queue.c
+++ sio_queue.c
@@ -1,13 +1,46 @@
 #include "sio_queue.h"
 #include <stdlib.h>
+#include <string.h>
+#include "sio_common.h"
 #include "sio_list.h"
+
+struct sio_queue
+{
+    struct sio_list_head head;
+    int count;
+};
 
 struct sio_queue *sio_queue_create()
 {
+    struct sio_queue *queue = malloc(sizeof(struct sio_queue));
+    SIO_COND_CHECK_RETURN_VAL(!queue, NULL);
+
+    memset(queue, 0, sizeof(struct sio_queue));
+
+    sio_list_init(&queue->head);
+
+    return queue;
+}
+
+int sio_queue_push(struct sio_queue *queue, struct sio_list_head *entry)
+{
+    SIO_COND_CHECK_RETURN_VAL(!queue, -1);
+
+    return -1;
+}
+
+struct sio_list_head *sio_queue_pop(struct sio_queue *queue)
+{
+    SIO_COND_CHECK_RETURN_VAL(!queue, NULL);
+
     return NULL;
 }
 
 int sio_queue_destory(struct sio_queue *queue)
 {
+    SIO_COND_CHECK_RETURN_VAL(!queue, -1);
+
+    free(queue);
+
     return 0;
 }
--- sio_queue.h
+++ sio_queue.h
@@ -1,6 +1,8 @@
 #ifndef SIO_QUEUE_H_
 #define SIO_QUEUE_H_
 
+#include "sio_list.h"
+
 struct sio_queue;
 
 #ifdef __cplusplus
@@ -8,6 +10,10 @@ extern "C" {
 #endif
 
 struct sio_queue *sio_queue_create();
+
+int sio_queue_push(struct sio_queue *queue, struct sio_list_head *entry);
+
+struct sio_list_head *sio_queue_pop(struct sio_queue *queue);
 
 int sio_queue_destory(struct sio_queue *queue);
 
