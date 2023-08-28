c4cbb49d4a10e63a9f42881ea6a0d199ecf376a4
zhaoyanghu <zhaoyang.hu3@gmail.com>
2023-08-28 17:47:05 +0800

    feat: sio_rwlock

--- sio_rwlock.h
+++ sio_rwlock.h
@@ -1,29 +1,30 @@
 #ifndef SIO_RWLOCK_H_
 #define SIO_RWLOCK_H_
 
-#ifdef LINUX
-#include <pthread.h>
-#elif defined(WIN32)
+#ifdef WIN32
 #include <windows.h>
+#else
+#include <pthread.h>
 #endif
 
-#ifdef LINUX
-#define sio_rwlock pthread_mutex_t
+#ifdef WIN32
+#define sio_rwlock SRWLOCK
+#define SIO_RWLOCK_INITIALIZER SRWLOCK_INIT
+#define sio_rwlock_init(rwlock) InitializeSRWLock(&rwlock)
+#define sio_rwlock_rdlock(rwlock) AcquireSRWLockShared(&rwlock)
+#define sio_rwlock_wrlock(rwlock) AcquireSRWLockExclusive(&rwlock)
+#define sio_rwlock_rdunlock(rwlock) ReleaseSRWLockShared(&rwlock)
+#define sio_rwlock_wrunlock(rwlock) ReleaseSRWLockExclusive(&rwlock)
+#define sio_rwlock_destory(rwlock)
+#else
+#define sio_rwlock pthread_rwlock_t
 #define SIO_RWLOCK_INITIALIZER PTHREAD_RWLOCK_INITIALIZER
-#define sio_rwlock_init(mutex) pthread_mutex_init(&mutex, NULL)
-#define sio_rwlock_lock(mutex) pthread_mutex_lock(&mutex)
-#define sio_rwlock_trylock(mutex) pthread_mutex_trylock(&mutex)
-#define sio_rwlock_unlock(mutex) pthread_mutex_unlock(&mutex)
-#define sio_rwlock_destory(mutex) pthread_mutex_destroy(&mutex)
-
-#elif defined(WIN32)
-#define sio_rwlock CRITICAL_SECTION
-#define SIO_RWLOCK_INITIALIZER { 0 }
-#define sio_rwlock_init(mutex) InitializeCriticalSection(&mutex)
-#define sio_rwlock_lock(mutex) EnterCriticalSection(&mutex)
-#define sio_rwlock_trylock(mutex) -1
-#define sio_rwlock_unlock(mutex) LeaveCriticalSection(&mutex)
-#define sio_rwlock_destory(mutex) DeleteCriticalSection(&mutex)
+#define sio_rwlock_init(rwlock) pthread_rwlock_init(&rwlock, NULL)
+#define sio_rwlock_rdlock(rwlock) pthread_rwlock_rdlock(&rwlock)
+#define sio_rwlock_wrlock(rwlock) pthread_rwlock_wrlock(&rwlock)
+#define sio_rwlock_rdunlock(rwlock) pthread_rwlock_unlock(&rwlock)
+#define sio_rwlock_wrunlock(rwlock) pthread_rwlock_unlock(&rwlock)
+#define sio_rwlock_destory(rwlock) pthread_rwlock_destroy(&rwlock)
 #endif
 
 #endif
